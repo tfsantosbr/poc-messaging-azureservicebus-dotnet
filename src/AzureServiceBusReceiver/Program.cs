bf5b6288bd03d13bfee2c4634d191145e9ad227c
Tiago Santos <tfsantosbr@gmail.com>
2024-08-12 10:21:07 -0300

    atualiza para app receiver/sender separados

---
+++ src/AzureServiceBusReceiver/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "Endpoint=sb://localhost/;SharedAccessKeyName=all;SharedAccessKey=CLwo3FQ3S39Z4pFOQDefaiUd1dSsli4XOAj3Y9Uh1E=;EnableAmqpLinkRedirect=false";
+string topicName = "sb-test-topic"; // Substitua pelo nome da fila
+
+// Crie um client para o Service Bus
+ServiceBusClient client = new(connectionString);
+
+// Função para receber mensagens
+async Task ReceiveMessagesAsync()
+{
+    ServiceBusReceiver receiver = client.CreateReceiver(topicName);
+
+    try
+    {
+        Console.WriteLine("Aguardando mensagens...");
+
+        while (true)
+        {
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+            if (receivedMessage != null)
+            {
+                string body = receivedMessage.Body.ToString();
+                Console.WriteLine($"Mensagem recebida: {body}");
+
+                // Complete a mensagem para que ela seja removida da fila
+                await receiver.CompleteMessageAsync(receivedMessage);
+            }
+        }
+    }
+    finally
+    {
+        await receiver.DisposeAsync();
+    }
+}
+
+await ReceiveMessagesAsync();
+
+await client.DisposeAsync();
---
+++ src/AzureServiceBusSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "Endpoint=sb://localhost/;SharedAccessKeyName=all;SharedAccessKey=CLwo3FQ3S39Z4pFOQDefaiUd1dSsli4XOAj3Y9Uh1E=;EnableAmqpLinkRedirect=false";
+string topicName = "sb-test-topic"; // Substitua pelo nome da fila
+
+// Crie um client para o Service Bus
+ServiceBusClient client = new(connectionString);
+
+// Função para enviar uma mensagem
+async Task SendMessageAsync(string messageBody)
+{
+    ServiceBusSender sender = client.CreateSender(topicName);
+    ServiceBusMessage message = new(messageBody);
+
+    try
+    {
+        await sender.SendMessageAsync(message);
+        Console.WriteLine($"Mensagem enviada: {messageBody}");
+    }
+    finally
+    {
+        await sender.DisposeAsync();
+    }
+}
+
+// Captura de mensagens do usuário
+while (true)
+{
+    Console.Write("Escreva uma mensagem (ou 'sair' para terminar): ");
+    string? input = Console.ReadLine();
+
+    if (input is null)
+    {
+        return;
+    }
+
+    if (input?.ToLower() == "sair")
+    {
+        break;
+    }
+
+    if (input is not null)
+    {
+        await SendMessageAsync(input);
+    }
+}
+
+await client.DisposeAsync();
